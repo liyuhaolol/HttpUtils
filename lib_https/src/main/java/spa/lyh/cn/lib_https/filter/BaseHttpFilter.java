b5221e10b67cb3e953127aa57c6ca7f08f50303b
liyuhao <liyuhaoid@sina.com>
2024-10-31 12:18:30 +0800

    更新1.5.4版本

--- lib_https/src/main/java/spa/lyh/cn/lib_https/filter/BaseHttpFilter.java
+++ lib_https/src/main/java/spa/lyh/cn/lib_https/filter/BaseHttpFilter.java
@@ -2,9 +2,11 @@ package spa.lyh.cn.lib_https.filter;
 
 import android.content.Context;
 
+import org.jetbrains.annotations.NotNull;
+
 import okhttp3.Headers;
 
 public abstract class BaseHttpFilter {
 
-    public abstract boolean dataFilter(Context context,String url, Headers headers, Object bodyData);
+    public abstract boolean dataFilter(@NotNull Context context,@NotNull  String url,@NotNull  Headers headers,@NotNull String stringBody);
 }
